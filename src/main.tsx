b112840c4a5a418280f67ac71a1393ab42521522
Rogério Rocha <neorogerio@gmail.com>
2022-09-13 10:13:13 -0300

    Aula 02 - Criando layout da landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App, Button } from './App'
+import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Button title="Send 1"/>
-    <Button title="Send 2"/>
-    <Button title="Send 3"/>
-    <Button title="Hello World"/>
+    <App />
   </React.StrictMode>
 )
